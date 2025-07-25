1c4c446e3d722795980db03dc5bbcb3ae1df3e8d
STR1006 <STR1006@gmail.com>
2025-07-24 22:34:39 -0400

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,42 +1,10 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import { VitePWA } from 'vite-plugin-pwa';
-import path from 'path';
 
+// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    VitePWA({
-      registerType: 'autoUpdate',
-      includeAssets: ['favicon.ico', 'pwa-icon-192.png', 'pwa-icon-512.png'],
-      manifest: {
-        name: 'Gulu Inventory PWA App',
-        short_name: 'Gulu PWA',
-        description: 'Gulu Inventory PWA App for managing shelf restocking',
-        theme_color: '#58ab7f',
-        background_color: '#ffffff',
-        display: 'standalone',
-        icons: [
-          {
-            src: '/pwa-icon-192.png',
-            sizes: '192x192',
-            type: 'image/png'
-          },
-          {
-            src: '/pwa-icon-512.png',
-            sizes: '512x512',
-            type: 'image/png'
-          }
-        ]
-      },
-      workbox: {
-        globPatterns: ['**/*.{js,css,html,ico,png,svg}'],
-      }
-    })
-  ],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
-    },
-  },
+  plugins: [react()],
+  // Set the base path for deployment to GitHub Pages.
+  // Replace 'gulu-pwa' with the actual name of your GitHub repository.
+  base: '/gulu-pwa/', 
 });
